31456844e943f6fef3a2d70f0f72f8b398ba8e34
grGrimales <grojasgrimales@gmail.com>
2022-09-08 21:26:02 -0500

    Se crea seccion de formacion

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,9 @@ export default function Home() {
       <section className={`${styles.section} ${styles.aboutMe}`}>
 
         <h2>About Me</h2>
-        <div className={styles.aboutMe__container}>
+
+        <div className={`${styles.container} ${styles.aboutMe__container}`}>
+
           <div className={styles.aboutMe__description}>
 
             <p>
@@ -47,202 +49,279 @@ export default function Home() {
 
         </div>
 
+
       </section>
 
       {/* Skills */}
 
       <section className={`${styles.section} ${styles.skills}`}>
         <h2>Herramientas</h2>
-        <div className={styles.skills__content}>
+        <div className={styles.container}>
+          <div className={styles.skills__content}>
 
-        <div className={styles.skills__data}>
-            <div className={styles.skills__image}>
-              <Image width={50} height={50} src='/images/figma.png' alt="Logo" />
-            </div>
-            <div className={styles.skills__description}>
-              <div className={styles.skills__title}>
-                <h3>Figma</h3>
-                <span>85%</span>
+            <div className={styles.skills__data}>
+              <div className={styles.skills__image}>
+                <Image width={50} height={50} src='/images/figma.png' alt="Logo" />
               </div>
+              <div className={styles.skills__description}>
+                <div className={styles.skills__title}>
+                  <h3>Figma</h3>
+                  <span>85%</span>
+                </div>
 
-              <div className={styles.skills__bar}>
-                <span className={`${styles.skills__percent} ${styles.skills__figma}`}></span>
+                <div className={styles.skills__bar}>
+                  <span className={`${styles.skills__percent} ${styles.skills__figma}`}></span>
+                </div>
               </div>
             </div>
+
+            <div className={styles.skills__data}>
+              <div className={styles.skills__image}>
+                <Image width={50} height={50} src='/images/canva.png' alt="Logo" />
+              </div>
+              <div className={styles.skills__description}>
+                <div className={styles.skills__title}>
+                  <h3>Canva</h3>
+                  <span>80%</span>
+                </div>
+
+                <div className={styles.skills__bar}>
+                  <span className={`${styles.skills__percent} ${styles.skills__canva}`}></span>
+                </div>
+              </div>
+            </div>
+
+
+            <div className={styles.skills__data}>
+              <div className={styles.skills__image}>
+                <Image width={50} height={50} src='/images/html5.png' alt="Logp" />
+              </div>
+              <div className={styles.skills__description}>
+                <div className={styles.skills__title}>
+                  <h3>HTML5</h3>
+                  <span>90%</span>
+                </div>
+
+                <div className={styles.skills__bar}>
+                  <span className={`${styles.skills__percent} ${styles.skills__html}`}></span>
+                </div>
+              </div>
+            </div>
+
+
+            <div className={styles.skills__data}>
+              <div className={styles.skills__image}>
+                <Image width={50} height={50} src='/images/css3.png' alt="Logo" />
+              </div>
+              <div className={styles.skills__description}>
+                <div className={styles.skills__title}>
+                  <h3>CSS3</h3>
+                  <span>85%</span>
+                </div>
+
+                <div className={styles.skills__bar}>
+                  <span className={`${styles.skills__percent} ${styles.skills__css}`}></span>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.skills__data}>
+              <div className={styles.skills__image}>
+                <Image width={50} height={50} src='/images/sass.png' alt="Logo" />
+              </div>
+              <div className={styles.skills__description}>
+                <div className={styles.skills__title}>
+                  <h3>SASS</h3>
+                  <span>85%</span>
+                </div>
+
+                <div className={styles.skills__bar}>
+                  <span className={`${styles.skills__percent} ${styles.skills__sass}`}></span>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.skills__data}>
+              <div className={styles.skills__image}>
+                <Image width={50} height={50} src='/images/bootstrap.png' alt="Logo" />
+              </div>
+              <div className={styles.skills__description}>
+                <div className={styles.skills__title}>
+                  <h3>Bootstrap</h3>
+                  <span>75%</span>
+                </div>
+
+                <div className={styles.skills__bar}>
+                  <span className={`${styles.skills__percent} ${styles.skills__bootstrap}`}></span>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.skills__data}>
+              <div className={styles.skills__image}>
+                <Image width={50} height={50} src='/images/js.png' alt="Logo" />
+              </div>
+              <div className={styles.skills__description}>
+                <div className={styles.skills__title}>
+                  <h3>JavaScript</h3>
+                  <span>85%</span>
+                </div>
+
+                <div className={styles.skills__bar}>
+                  <span className={`${styles.skills__percent} ${styles.skills__js}`}></span>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.skills__data}>
+              <div className={styles.skills__image}>
+                <Image width={50} height={50} src='/images/react.png' alt="Logo" />
+              </div>
+              <div className={styles.skills__description}>
+                <div className={styles.skills__title}>
+                  <h3>React</h3>
+                  <span>85%</span>
+                </div>
+
+                <div className={styles.skills__bar}>
+                  <span className={`${styles.skills__percent} ${styles.skills__react}`}></span>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.skills__data}>
+              <div className={styles.skills__image}>
+                <Image width={50} height={50} src='/images/firebase.png' alt="Logo" />
+              </div>
+              <div className={styles.skills__description}>
+                <div className={styles.skills__title}>
+                  <h3>Firebase</h3>
+                  <span>80%</span>
+                </div>
+
+                <div className={styles.skills__bar}>
+                  <span className={`${styles.skills__percent} ${styles.skills__firebase}`}></span>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.skills__data}>
+              <div className={styles.skills__image}>
+                <Image width={50} height={50} src='/images/git.png' alt="Logo" />
+              </div>
+              <div className={styles.skills__description}>
+                <div className={styles.skills__title}>
+                  <h3>Git</h3>
+                  <span>80%</span>
+                </div>
+
+                <div className={styles.skills__bar}>
+                  <span className={`${styles.skills__percent} ${styles.skills__git}`}></span>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.skills__data}>
+              <div className={styles.skills__image}>
+                <Image width={50} height={50} src='/images/github.png' alt="Logo" />
+              </div>
+              <div className={styles.skills__description}>
+                <div className={styles.skills__title}>
+                  <h3>Github</h3>
+                  <span>80%</span>
+                </div>
+
+                <div className={styles.skills__bar}>
+                  <span className={`${styles.skills__percent} ${styles.skills__github}`}></span>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+
           </div>
-
-          <div className={styles.skills__data}>
-            <div className={styles.skills__image}>
-              <Image width={50} height={50} src='/images/canva.png' alt="Logo" />
-            </div>
-            <div className={styles.skills__description}>
-              <div className={styles.skills__title}>
-                <h3>Canva</h3>
-                <span>80%</span>
-              </div>
-
-              <div className={styles.skills__bar}>
-                <span className={`${styles.skills__percent} ${styles.skills__canva}`}></span>
-              </div>
-            </div>
-          </div>
-
-
-          <div className={styles.skills__data}>
-            <div className={styles.skills__image}>
-              <Image width={50} height={50} src='/images/html5.png' alt="Logp" />
-            </div>
-            <div className={styles.skills__description}>
-              <div className={styles.skills__title}>
-                <h3>HTML5</h3>
-                <span>90%</span>
-              </div>
-
-              <div className={styles.skills__bar}>
-                <span className={`${styles.skills__percent} ${styles.skills__html}`}></span>
-              </div>
-            </div>
-          </div>
-
-
-          <div className={styles.skills__data}>
-            <div className={styles.skills__image}>
-            <Image width={50} height={50} src='/images/css3.png' alt="Logo" />
-            </div>
-            <div className={styles.skills__description}>
-              <div className={styles.skills__title}>
-                <h3>CSS3</h3>
-                <span>85%</span>
-              </div>
-
-              <div className={styles.skills__bar}>
-                <span className={`${styles.skills__percent} ${styles.skills__css}`}></span>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.skills__data}>
-            <div className={styles.skills__image}>
-              <Image width={50} height={50} src='/images/sass.png' alt="Logo" />
-            </div>
-            <div className={styles.skills__description}>
-              <div className={styles.skills__title}>
-                <h3>SASS</h3>
-                <span>85%</span>
-              </div>
-
-              <div className={styles.skills__bar}>
-                <span className={`${styles.skills__percent} ${styles.skills__sass}`}></span>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.skills__data}>
-            <div className={styles.skills__image}>
-              <Image width={50} height={50} src='/images/bootstrap.png' alt="Logo" />
-            </div>
-            <div className={styles.skills__description}>
-              <div className={styles.skills__title}>
-                <h3>Bootstrap</h3>
-                <span>75%</span>
-              </div>
-
-              <div className={styles.skills__bar}>
-                <span className={`${styles.skills__percent} ${styles.skills__bootstrap}`}></span>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.skills__data}>
-            <div className={styles.skills__image}>
-              <Image width={50} height={50} src='/images/js.png' alt="Logo" />
-            </div>
-            <div className={styles.skills__description}>
-              <div className={styles.skills__title}>
-                <h3>JavaScript</h3>
-                <span>85%</span>
-              </div>
-
-              <div className={styles.skills__bar}>
-                <span className={`${styles.skills__percent} ${styles.skills__js}`}></span>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.skills__data}>
-            <div className={styles.skills__image}>
-              <Image width={50} height={50} src='/images/react.png' alt="Logo" />
-            </div>
-            <div className={styles.skills__description}>
-              <div className={styles.skills__title}>
-                <h3>React</h3>
-                <span>85%</span>
-              </div>
-
-              <div className={styles.skills__bar}>
-                <span className={`${styles.skills__percent} ${styles.skills__react}`}></span>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.skills__data}>
-            <div className={styles.skills__image}>
-              <Image width={50} height={50} src='/images/firebase.png' alt="Logo" />
-            </div>
-            <div className={styles.skills__description}>
-              <div className={styles.skills__title}>
-                <h3>Firebase</h3>
-                <span>80%</span>
-              </div>
-
-              <div className={styles.skills__bar}>
-                <span className={`${styles.skills__percent} ${styles.skills__firebase}`}></span>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.skills__data}>
-            <div className={styles.skills__image}>
-              <Image width={50} height={50} src='/images/git.png' alt="Logo" />
-            </div>
-            <div className={styles.skills__description}>
-              <div className={styles.skills__title}>
-                <h3>Git</h3>
-                <span>80%</span>
-              </div>
-
-              <div className={styles.skills__bar}>
-                <span className={`${styles.skills__percent} ${styles.skills__git}`}></span>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.skills__data}>
-            <div className={styles.skills__image}>
-              <Image width={50} height={50} src='/images/github.png' alt="Logo" />
-            </div>
-            <div className={styles.skills__description}>
-              <div className={styles.skills__title}>
-                <h3>Github</h3>
-                <span>80%</span>
-              </div>
-
-              <div className={styles.skills__bar}>
-                <span className={`${styles.skills__percent} ${styles.skills__github}`}></span>
-              </div>
-            </div>
-          </div>
-
-
-
-
-
-
         </div>
+
 
       </section>
 
+      {/* Formation */}
 
+      <section className={`${styles.section} ${styles.formation}`}>
+        <h2>Estudios</h2>
+        <div className={`${styles.container} ${styles.formation__container}`}>
+          <div className={styles.formation__all}>
+            <div className={styles.formation__content}>
+              <div className={styles.formation__data}>
+                <div>
+                  <h3 className={styles.formation__name}>Frontend Developer</h3>
+                  <span className={styles.formation__academy}>CoderHouse</span>
+                  <div className={styles.formation__date}>
+                    <p>2021 - 2022</p>
+                  </div>
+                </div>
+                <div>
+                  <span className={styles.formation__circle}></span>
+                  <span className={styles.formation__line}></span>
+
+                </div>
+              </div>
+
+              <div className={styles.formation__data}>
+                <div></div>
+                <div>
+                  <span className={styles.formation__circle}></span>
+                  <span className={styles.formation__line}></span>
+
+                </div>
+                <div>
+                  <h3 className={styles.formation__name}>Next.js</h3>
+                  <span className={styles.formation__academy}>Udemy</span>
+                  <div className={styles.formation__date}>
+                    <p> 2022</p>
+                  </div>
+                </div>
+               
+              </div>
+
+              <div className={styles.formation__data}>
+                <div>
+                  <h3 className={styles.formation__name}>Next.js</h3>
+                  <span className={styles.formation__academy}>Udemy</span>
+                  <div className={styles.formation__date}>
+                    <p> 2022</p>
+                  </div>
+                </div>
+                <div>
+                  <span className={styles.formation__circle}></span>
+                  <span className={styles.formation__line}></span>
+
+                </div>
+              </div>
+
+              <div className={styles.formation__data}>
+                <div></div>
+                <div>
+                  <span className={styles.formation__circle}></span>
+                  <span className={styles.formation__line}></span>
+
+                </div>
+                <div>
+                  <h3 className={styles.formation__name}>Next.js</h3>
+                  <span className={styles.formation__academy}>Udemy</span>
+                  <div className={styles.formation__date}>
+                    <p> 2022</p>
+                  </div>
+                </div>
+               
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 }
